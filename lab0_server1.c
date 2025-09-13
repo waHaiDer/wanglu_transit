7d90a6a15bbf5d9d211a67289ce83d208b1e933e
waHaiDer <winstondavlim4@gmail.com>
2025-09-13 20:59:22 +0800

    Update lab0_server1.c

--- lab0_server1.c
+++ lab0_server1.c
@@ -1,8 +1,8 @@
-// server1.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 12345
 #define BUFSIZE 1024
@@ -14,28 +14,13 @@ int main() {
     socklen_t addr_len = sizeof(client_addr);
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        exit(1);
-    }
-
     printf("Server listening on port %d...\n", PORT);
 
     int n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
-    if (n < 0) {
-        perror("Receive failed");
-        exit(1);
-    }
-
     buffer[n] = '\0';
     printf("Received student ID: %s\n", buffer);
 
