d0b92ac7dd97c734059539900375d738568fdfdb
waHaiDer <winstondavlim4@gmail.com>
2025-09-13 20:33:32 +0800

    Create lab0_client1.c

---
+++ lab0_client1.c
@@ -0,0 +1,35 @@
+// client1.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFSIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char message[BUFSIZE] = "D1133813"; 
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int n = sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (n < 0) {
+        perror("Send failed");
+        exit(1);
+    }
+
+    printf("Student ID sent: %s\n", message);
+
+    close(sockfd);
+    return 0;
+}
