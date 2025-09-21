ae7152abf71b904057a7fb07d07ea52375788c94
waHaiDer <winstondavlim4@gmail.com>
2025-09-21 23:09:40 +0800

    Update lab1hw_client.c

--- lab1hw_client.c
+++ lab1hw_client.c
@@ -34,63 +34,64 @@ static ssize_t recv_line(int fd, char *buf, size_t cap) {
     buf[used] = '\0';
     return (ssize_t)used;
 }
+
 static void transform_input(const char *in, char *out, size_t out_cap) {
-    out[0] = '\0';
-    size_t L = 0;
+    size_t L = 0; out[0] = '\0';
     for (size_t i = 0; in[i]; ++i) {
         unsigned char ch = (unsigned char)in[i];
-        if (isalnum(ch)) {
-            char token[4];
-            if (isalpha(ch)) token[0] = (char)toupper(ch);
-            else token[0] = (char)ch;
-            token[1] = '\0';
-
-            if (L > 0 && L + 1 < out_cap) { out[L++] = ' '; out[L] = '\0'; }
-            if (L + 1 < out_cap) { out[L++] = token[0]; out[L] = '\0'; }
-        }
+        if (!isalnum(ch)) continue;
+        char t = isalpha(ch) ? (char)toupper(ch) : (char)ch;
+        if (L && L + 1 < out_cap) out[L++] = ' ';
+        if (L + 1 < out_cap) out[L++] = t;
+        out[L] = '\0';
     }
     if (L + 1 < out_cap) { out[L++] = '\n'; out[L] = '\0'; }
 }
 
 int main(void) {
+    // 1) create and connect
     int s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0) { perror("socket"); exit(1); }
 
-    struct sockaddr_in srv;
-    memset(&srv, 0, sizeof(srv));
+    struct sockaddr_in srv; memset(&srv, 0, sizeof(srv));
     srv.sin_family = AF_INET;
     srv.sin_port   = htons(PORT);
     if (inet_pton(AF_INET, SERVER_IP, &srv.sin_addr) != 1) {
         fprintf(stderr, "inet_pton failed for %s\n", SERVER_IP);
         close(s); exit(1);
     }
-
     if (connect(s, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
-        perror("connect");
-        close(s); exit(1);
+        perror("connect"); close(s); exit(1);
     }
 
-    printf("Connected. Enter lines (Ctrl+D to quit):\n");
+    printf("Connected to server:\n");
 
-    char inbuf[MAXLINE];
-    char sendbuf[MAXLINE];
-    char recvbuf[MAXLINE];
+    char inbuf[MAXLINE], sendbuf[MAXLINE], recvbuf[MAXLINE];
 
     while (fgets(inbuf, sizeof(inbuf), stdin) != NULL) {
         inbuf[strcspn(inbuf, "\n")] = '\0';
+        printf("%s\n", inbuf);
+
         transform_input(inbuf, sendbuf, sizeof(sendbuf));
+        {
+            char show[MAXLINE];
+            strncpy(show, sendbuf, sizeof(show));
+            show[sizeof(show)-1] = '\0';
+            show[strcspn(show, "\n")] = '\0';
+            printf("Sent: %s\n", show);
+        }
+
         if (send_all(s, sendbuf, strlen(sendbuf)) < 0) {
             perror("send"); close(s); exit(1);
         }
+
         ssize_t n = recv_line(s, recvbuf, sizeof(recvbuf));
         if (n < 0) { perror("recv"); close(s); exit(1); }
-        if (n == 0) {
-            printf("Server closed connection.\n");
-            close(s);
-            return 0;
-        }
-        fputs(recvbuf, stdout);
+        if (n == 0) { printf("Server closed connection.\n"); close(s); return 0; }
+
+        fputs(recvbuf, stdout); 
     }
+    printf("EOF detected. Closing connection.\n");
     close(s);
     return 0;
 }
